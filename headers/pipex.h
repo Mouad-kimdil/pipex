18e3319597d906b5dd3e2d79564efb060872b20f
Mouad Kimdil <mkimdil@e1r5p13.1337.ma>
2024-02-02 13:41:47 +0100

    header directory containing pipex mandatory header file and pipex bonus header file

---
+++ headers/pipex.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkimdil <mkimdil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 15:11:17 by mkimdil           #+#    #+#             */
+/*   Updated: 2024/01/08 18:04:45 by mkimdil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+int		countword(const char *s, char c);
+char	*ft_strndup(const char *src, int n);
+void	ft_free(char **ptr, int i);
+char	**ft_help(const char *s, char c, int len, char **final);
+char	**ft_split(const char *s, char c);
+int		ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*findpath(char **env);
+void	ft_putstr(char *str, int fd);
+int		err_exit(char *s, char *cmd, char **args);
+void	free_array(char **str);
+void	fatal(char *msg);
+char	*check_access(char **path, char *cmd);
+int		ft_strncmp(const char *s1, const char *s2, int len);
+void	sub_dup2(int *fds, int fd, int state);
+
+#endif
---
+++ headers/pipex_bonus.h
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkimdil <mkimdil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 18:16:50 by mkimdil           #+#    #+#             */
+/*   Updated: 2024/01/11 17:39:18 by mkimdil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 33
+# endif
+
+typedef struct s_pipex
+{
+	int		infile;
+	int		outfile;
+	char	*env_path;
+	char	**cmd_paths;
+	char	*cmd;
+	char	**cmd_args;
+	pid_t	pid;
+	int		here_doc;
+	int		cmd_nmbs;
+	int		pipe_nmbs;
+	int		*pipe;
+	int		idx;
+}	t_pipex;
+
+int		countword(const char *s, char c);
+char	*ft_strndup(const char *src, int n);
+void	ft_free(char **ptr, int i);
+char	**ft_help(const char *s, char c, int len, char **final);
+char	**ft_split(const char *s, char c);
+int		ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strncmp(const char *s1, const char *s2, int len);
+void	ft_putstr(char *str, int fd);
+char	*findpath(char **env);
+void	err_msg(char *msg);
+void	get_infile(char **av, t_pipex *pipe);
+void	get_outfile(char **av, int ac, t_pipex *pipe);
+char	*check_access(char **path, char *cmd);
+void	child(t_pipex pipex, char **argv, char **envp);
+void	creat_pipes(t_pipex *pipex);
+void	free_all(t_pipex *pipex);
+void	close_pipes(t_pipex *pipex);
+void	child_free(t_pipex *pipex);
+void	err_msg2(char *s, char *cmd);
+void	free_array(char **arr);
+void	sub_dup2(int fd1, int fd2);
+void	main_free(t_pipex *pipex);
+int		check(char	**av, t_pipex *pipex);
+void	err_msg3(t_pipex pipex);
+char	*ft_join(char *buffer, char *buf);
+char	*ft_next_line(char *buffer);
+char	*ft_get_line(char *buffer);
+char	*ft_read_file(int fd, char *res);
+char	*get_next_line(int fd);
+char	*ft_strrchr(char *str, int c);
+void	*ft_calloc(size_t count, size_t size);
+void	here_doc(char *delimiter, t_pipex *pipex);
+
+#endif
